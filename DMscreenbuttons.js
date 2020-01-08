9a1d99949c67206ad5fb1ca0aa3805e6afad8f33
jbmccormick <jonathan.b.mccormick@gmail.com>
2020-01-08 14:52:37 -0600

    Update DMscreenbuttons.js

--- DMscreenbuttons.js
+++ DMscreenbuttons.js
@@ -6,6 +6,7 @@
 // @author       You
 // @match        https://5e.tools/dmscreen.html
 // @grant        GM_addStyle
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
@@ -16,21 +17,18 @@ const buttonsDM = [
     ["Button3","Btn3"],
 ];
 var divNew = document.createElement('div');
-var mainViewport = document.getElementsByClassName("viewport-wrapper")[0];
-var controlsElement = document.getElementsByClassName("dm-init-wrp-controls")[0];
-//console.log(controlsElement);
+var pageHeader = document.getElementsByClassName("hidden-xs hidden-sm page__header")[0];
     divNew.id = "buttonPaneOne";
 var buttonDivOne = document.getElementById("buttonPaneOne");
-//    mainViewport.insertAdjacentElement("beforebegin", divNew);
-    controlsElement.insertAdjacentElement("beforebegin", divNew);
-for(let i = 0; i < buttonsDM.length; i++){ //creating buttons for each program area
+    pageHeader.insertAdjacentElement("afterend", divNew);
+for(let i = 0; i < buttonsDM.length; i++){
 	let btnDM = document.createElement('button');
 	btnDM.style = "z-index: 1100 !important;"
 	btnDM.innerHTML = buttonsDM[i][0];
-	btnDM.id = buttonsDM[i][1];
+	btnDM.id = buttonsDM[i][0];
 	btnDM.type = 'button';
 	divNew.appendChild(btnDM);
-	btnDM.addEventListener("click", function() { buttonsDM[i][0] });
+	btnDM.addEventListener("click", window[this.id]());
 };
     function Button1() {
         alert("Button1 pushed");
@@ -41,4 +39,4 @@ for(let i = 0; i < buttonsDM.length; i++){ //creating buttons for each program a
     function Button3() {
         alert("Button3 pushed");
     }
-})();
+();
